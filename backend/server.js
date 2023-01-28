9bb8b259d931f2ff8ebd321a7e18b11418f40c59
Hans Raj <hans63565@gmail.com>
2023-01-29 00:40:37 +0500

    setup api request routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,24 @@
+require('dotenv').config()
 const express = require('express')
-
+const workoutRoutes = require('./routes/workouts')
 
 //express app
 const app = express()
 
+
+//middleware
+app.use(express.json())
+
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
+
+//routes
+app.use('/api/workouts',workoutRoutes)
+
+
 //listener for requests
-app.listen(4000,()=>{
-    console.log("listening on port 4000")
+app.listen(process.env.PORT,()=>{
+    console.log("listening on port",process.env.PORT)
 })
