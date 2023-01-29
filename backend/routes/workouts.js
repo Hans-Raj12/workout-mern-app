2ab4e5743a39e97cd6b079a063f9325fa076620e
Hans Raj <hans63565@gmail.com>
2023-01-29 20:09:41 +0500

    added controllers for workout request like GET, POST, DELETE & PATCH. With this completed the backend of the project

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,38 +1,27 @@
 const express = require('express')
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController')
 
 const router = express.Router()
 
 //GET all workout
-router.get('/',(req,res)=>{
-    res.json({msg:"GET all workouts"})
-})
+router.get('/',getWorkouts)
 
 //GET a single workout
-router.get('/:id',(req,res)=>{
-    res.json({
-        msg : "GET a single workout"
-    })
-})
+router.get('/:id',getWorkout)
 
 // POST a new workout
-router.post('/',(req,res)=>{
-    res.json({
-        msg:"POST a new workout"
-    })
-})
+router.post('/',createWorkout)
 
 //DELETE a workout
-router.delete('/:id',(req,res)=>{
-    res.json({
-        msg:"DELETE a workout"
-    })
-})
+router.delete('/:id',deleteWorkout)
 
 //UPDATE a workout
-router.patch('/',(req,res)=>{
-    res.json({
-        msg:"UPDATE a workout"
-    })
-})
+router.patch('/:id',updateWorkout)
 
 module.exports = router
