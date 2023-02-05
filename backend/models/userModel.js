1be84fcaa48a2e4294a9edae464e6334ebbb5778
Hans Raj <hans63565@gmail.com>
2023-02-05 15:56:14 +0500

    added the signup functionality and also added bcrypt package to hash the password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-
-const Scehema = mongoose.Schema
+const bcrypt = require('bcrypt')
+const Schema = mongoose.Schema
 
 const userSchema = new Schema({
     email:{
@@ -13,5 +13,26 @@ const userSchema = new Schema({
         required:true
     }
 })
+
+//singup static method
+userSchema.statics.signup = async function(email, password){
+
+    const exists = await this.findOne({email})
+
+    if(exists){
+        throw Error('Email already in use')
+    }
+     
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password,salt)
+
+    const user = await this.create({email,password:hash})
+
+    return user
+
+}
+
+
+//login static method
 
 module.exports = mongoose.model('User',userSchema)
