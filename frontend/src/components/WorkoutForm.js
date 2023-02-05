1577c827440b0cecdaaccd6ed2ccd2bd9c214884
Hans Raj <hans63565@gmail.com>
2023-02-06 01:05:19 +0500

    Making authorized requests

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+import { useAuthContext } from "../hooks/useAuthContext"
+
 const WorkoutForm = () => {
     const {dispatch} = useWorkoutsContext()
     const [title, setTitle ] = useState("")
@@ -7,17 +9,23 @@ const WorkoutForm = () => {
     const [reps, setReps ] = useState("")
     const [error, setError] = useState(null)
     const [emptyFields, setEmptyFields] = useState([])
+    const {user} = useAuthContext()
     
     const handleSubmit = async (e)=>{
         e.preventDefault()
 
+        if(!user){
+            setError('you must be logged in')
+            return
+        }
         const workout = {title, load, reps}
         
         const response = await fetch('/api/workouts',{
             method : 'POST',
             body : JSON.stringify(workout),
             headers : {
-                'Content-Type':'application/json'
+                'Content-Type':'application/json',
+                'Authorization':`Bearer ${user.token}`
             }
         })
 
